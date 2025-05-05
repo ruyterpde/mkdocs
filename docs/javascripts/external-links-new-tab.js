2eeeb86e8a88481929e2bfbc618b12a3338267d0
unknown <Administrator@HOME.LOCAL>
2025-05-05 17:18:38 +0200

    Stop tracking the includes folder

--- docs/javascripts/external-links-new-tab.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  document.querySelectorAll("a[href^='http']:not([href*='ruyter.org'])").forEach(function (el) {
-    el.setAttribute("target", "_blank");
-    el.setAttribute("rel", "noopener noreferrer");
-  });
-});
